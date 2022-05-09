a2b079fc069ded8b29d7139cb4b3323866fb4e21
EsraaHamada100 <esraahamada654@gmail.com>
2022-05-10 01:04:11 +0200

    e

--- check_login_data.php
+++ check_login_data.php
@@ -1,5 +1,26 @@
 <?php 
 require_once "global.php";
-databaseConnection();
-echo $_POST['userName'];
-echo $_POST['password'];
+//  echo $connection ;
+function findUser($tableName, $pageLink){
+    $sql = "SELECT * FROM $tableName;";
+    // show you some informatin about your table
+    $result = mysqli_query($GLOBALS['connection'], $sql);
+    if(mysqli_num_rows($result) > 0){
+        // mysqli_fetch_array($result) give you the data row by row
+        while($row = mysqli_fetch_array($result)){
+            if($row['username'] == $_POST["userName"] && $row['password'] == $_POST["password"]){
+                // navigate to this page
+                header("Location: $pageLink");
+                exit();
+            }
+        }
+    }
+}
+findUser('admin', 'index.html');
+findUser('teacher','teacher.html');
+findUser('co_teacher', 'co_teacher.html');
+findUser('student', 'student.html');
+header("Location: login.php");
+
+// echo $_POST['userName'];
+// echo $_POST['password'];
--- global.php
+++ global.php
@@ -1,10 +1,10 @@
 <?php
-function databaseConnection(){
-    $dbServerName = "localhost";
-    $dbUserName = "root";
-    $dbPassword = "";
-    $dbName = "automated_teacher_evaluation";
-    $connection = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
-}
+// database variables
+$dbServerName = "localhost";
+$dbUserName = "root";
+$dbPassword = "";
+$dbName = "automated_teacher_evaluation";
+$connection = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+
 
 ?>
